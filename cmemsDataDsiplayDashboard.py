7b72a9a4b3bbd12a77a15e735e278c78ed01e162
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2023-11-03 15:14:35 +0100

    dashboard sample

--- cmemsDataDsiplayDashboard.py
+++ cmemsDataDsiplayDashboard.py
@@ -1,60 +1,53 @@
-import requests
-import plotly.graph_objs as go
 import dash
+import plotly.graph_objs as go
 from dash import html, dcc
-import base64
+import dash_bootstrap_components as dbc
 
 # Create a Dash app
 app = dash.Dash(__name__)
 
-# The URL of the WMS service
-url = "https://my.cmems-du.eu/thredds/wms/cmems_mod_blk_wav_my_2.5km_PT1H-i"
-
-# Define the parameters for the WMS request
+# The URL of the ECMWF
+ecmwfurl = "https://charts.ecmwf.int/opencharts-api/v1/products/opencharts_meteogram"
+scale_factor = 0.5
+image_url = 'https://charts.ecmwf.int/content/20231103081456-124b11b5e5a4ba9aa1a1d6a5dfc077f7be2c8648.png'
+# Define the parameters for the ECMWF request
 params = {
-    "request": "GetMap",
-    "service": "WMS",
-    "VERSION": "1.3.0",
-    "LAYERS": "VMDR_WW",
-    "CRS": "EPSG:4326",
-    "BBOX": "27,41.75,28.75,44",  # Corrected BBOX
-    "WIDTH": "100",
-    "HEIGHT": "100",
-    "STYLES": "",  # You can specify styles if needed
-    "FORMAT": "image/png"
+    "format": "png",
+    "base_time": "2023-11-03T00%3A00%3A00Z",
+    "epsgram": "classical_wave",
+    "lon": "28.75",
+    "lat": "44",
 }
 
-# Send the HTTP GET request
-response = requests.get(url, params=params)
 
-if response.status_code == 200:
-    imgsource = base64.b64encode(response.content).decode('utf-8')
-    fig1 = go.Figure(go.Scattermapbox(), layout=dict(width=1000, height=700))
-    mapboxt = 'YOUR_MAPBOX_TOKEN'  # Replace with your Mapbox token
-    mapbox = dict(
-        zoom=4.5,
-        accesstoken=mapboxt,
-        style='light',  # Set your preferred Mapbox style
-        center=dict(lat=42.375, lon=27.875),  # Adjust center accordingly
-        layers=[
-            dict(
-                source=imgsource,
-                sourcetype="image",
-                coordinates=[
-                    [27, 41.75], [28.75, 41.75], [28.75, 44], [27, 44]
-                ])
-        ])
 
-    fig1.update_layout(mapbox=mapbox)
 
-    app.layout = html.Div([
+
+fig1 = go.Figure(go.Scattermapbox(), layout=dict(width=1000, height=700))
+mapboxt = 'pk.eyJ1Ijoicm1vdHdhbmkiLCJhIjoiY2xvNDVndHY2MDRlejJ4czIwa3QyYnk2bCJ9.g88Jq0lCZRcQda4eNPks2Q'
+mapbox = dict(
+    zoom=4.5,
+    accesstoken=mapboxt,
+    center=dict(lat=44, lon=28.75),
+
+)
+
+fig1.update_layout(margin=dict(l=0, r=0, t=0, b=0),
+                   legend=dict(x=0.01, y=0.05, traceorder="normal"),
+                   mapbox_style="open-street-map"
+                   )
+fig1.update_layout(mapbox=mapbox)
+
+
+app.layout = html.Div([
         dcc.Graph(
-            id='example-graph',
+            id='cmems-grph',
             figure=fig1
-        )
-    ])
-else:
-    print(f"Request failed with status code {response.status_code}")
+        ),
+    html.Img(src='https://charts.ecmwf.int/content/20231103081456-124b11b5e5a4ba9aa1a1d6a5dfc077f7be2c8648.png')
+],style={'display': 'flex', 'flex-direction': 'row'})
+
+#get_ecmwf_data()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
