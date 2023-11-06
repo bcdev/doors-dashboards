d5f1f447b337d336f754faefa84f15ad2384de27
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2023-11-06 10:33:48 +0100

    fit to window html controls

--- cmemsDataDsiplayDashboard.py
+++ cmemsDataDsiplayDashboard.py
@@ -23,7 +23,7 @@ params = {
 
 
 
-fig1 = go.Figure(go.Scattermapbox(), layout=dict(width=1000, height=700))
+fig1 = go.Figure(go.Scattermapbox())
 mapboxt = 'pk.eyJ1Ijoicm1vdHdhbmkiLCJhIjoiY2xvNDVndHY2MDRlejJ4czIwa3QyYnk2bCJ9.g88Jq0lCZRcQda4eNPks2Q'
 mapbox = dict(
     zoom=4.5,
@@ -38,14 +38,13 @@ fig1.update_layout(margin=dict(l=0, r=0, t=0, b=0),
                    )
 fig1.update_layout(mapbox=mapbox)
 
-
 app.layout = html.Div([
         dcc.Graph(
             id='cmems-grph',
             figure=fig1
         ),
     html.Img(src='https://charts.ecmwf.int/content/20231103081456-124b11b5e5a4ba9aa1a1d6a5dfc077f7be2c8648.png')
-],style={'display': 'flex', 'flex-direction': 'row'})
+],style={'display': 'flex', 'flex-direction': 'row', 'width': '100%', 'height': '100vh'})
 
 #get_ecmwf_data()
 
