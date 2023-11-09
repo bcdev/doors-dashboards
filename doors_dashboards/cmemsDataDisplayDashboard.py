1cd4c36cf4e61ad0165416876c2115d82f9f2b55
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2023-11-09 13:31:53 +0100

    multiple markers

--- doors_dashboards/cmemsDataDisplayDashboard.py
+++ doors_dashboards/cmemsDataDisplayDashboard.py
@@ -2,6 +2,7 @@ import dash
 import plotly.graph_objs as go
 from dash import html, dcc
 import requests
+from datetime import datetime
 
 # Create a Dash app
 app = dash.Dash(__name__)
@@ -10,11 +11,14 @@ app = dash.Dash(__name__)
 ecmwfurl = "https://charts.ecmwf.int/opencharts-api/v1/products/opencharts_meteogram/"
 scale_factor = 0.5
 image_url = ''
+current_date = datetime.now().strftime("%Y-%m-%dT00:00:00Z")
+
 # Define the parameters for the ECMWF request
 headers = {"accept": "application/json"}
+
 params = {
     "format": "png",
-    "base_time": "2023-11-08T00:00:00Z",
+    "base_time": current_date,
     "epsgram": "classical_wave",
     "lon": "28",
     "lat": "42.6",
@@ -31,18 +35,18 @@ def get_ecmwf_data():
         #print(response_data)
 
 
-point_lat = 42.6
-point_lon = 28
+point_lat = [42.6,42.525,42.47,42.655]
+point_lon = [28,27.7,27.46,27.725]
 
 #get_ecmwf_data()
 
-fig1 = go.Figure(go.Scattermapbox(lat=[point_lat],
-    lon=[point_lon],
+fig1 = go.Figure(go.Scattermapbox(lat=[42.6,42.525,42.47,42.655],
+    lon=[28,27.7,27.46,27.725],
     mode='markers',
     marker=go.scattermapbox.Marker(
         size=9,color='blue'
     ),
-    text='Bulgarian Coastline',))
+    text=['Bulgarian Coastline','Burgas Bay','Burgas Port','Passenger Terminal in Nessebar']))
 mapboxt = 'pk.eyJ1Ijoicm1vdHdhbmkiLCJhIjoiY2xvNDVndHY2MDRlejJ4czIwa3QyYnk2bCJ9.g88Jq0lCZRcQda4eNPks2Q'
 mapbox = dict(
     zoom=4.5,
@@ -63,7 +67,7 @@ app.layout = html.Div([
             figure=fig1
         ),
     html.Img(src=get_ecmwf_data())
-],style={'display': 'flex', 'flex-direction': 'row', 'width': '100%', 'height': '100vh'})
+],style={'display': 'flex', 'flexDirection': 'row', 'width': '100%', 'height': '100vh'})
 
 
 
