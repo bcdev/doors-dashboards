43ba03f639cd43b7ef1cb22a84f3219ff01bdb75
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2023-11-20 16:05:31 +0100

    Structure changes and updating meteogram on point selection

--- doors_dashboards/dashboards/bulgariaportsdashboard.py
+++ doors_dashboards/dashboards/bulgariaportsdashboard.py
@@ -1,4 +1,4 @@
-from dash import html
+from dash import html, Input, Output
 from dash import Dash
 
 from doors_dashboards.components.scattermap import ScatterMapComponent
@@ -14,6 +14,7 @@ BULGARIA_PORTS_POINTS = [
     (27.728, 42.657, 'Terminal Nessebar'),
     (27.687, 42.42, 'Terminal Sozopol')
 ]
+METEROGRAM_ID = 'ecmwf-img'
 
 
 def _create_app() -> Dash:
@@ -21,17 +22,42 @@ def _create_app() -> Dash:
 
     scattermap = ScatterMapComponent().get(DASHBOARD_ID, BULGARIA_PORTS_POINTS)
     coastline_central = BULGARIA_PORTS_POINTS[0]
+    marker_label_default = coastline_central[2]
     meteogram = MeteogramComponent().get(coastline_central[0], coastline_central[1])
 
     app.layout = html.Div(
-        [scattermap, meteogram],
+        [scattermap,
+         html.Div(
+             id=METEROGRAM_ID,
+             children=[
+                 html.Label(
+                     id='marker-label',
+                     children=marker_label_default,
+                     style={'textAlign': 'center', 'fontSize': '24px', 'fontWeight': 'bold', 'marginBottom': '10px'}
+                 ),
+                 meteogram
+             ]
+         )],
         style={
             'display': 'flex',
-            'flex-direction': 'row',
+            'flexDirection': 'row',
             'width': '100%',
             'height': '100vh'
         }
     )
+
+    @app.callback(Output('marker-label', 'children'),
+                  Output(METEROGRAM_ID, 'children'),
+                  Input(DASHBOARD_ID, 'clickData')
+                  )
+    def update_ecmwf_image(click_data):
+        print(marker_label_default)
+        if click_data is None:
+            marker_label = marker_label_default
+            return marker_label,meteogram
+        else:
+            marker_label = click_data['points'][0]['text']
+            return marker_label, MeteogramComponent().get(click_data['points'][0]['lon'], click_data['points'][0]['lat'])
 
     return app
 
