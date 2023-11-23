7db47ddd9f6d3b0bb071d39c80e6414a9850b2d9
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2023-11-23 20:07:45 +0100

    scattermap changes

--- doors_dashboards/dashboards/geodbdashboard.py
+++ doors_dashboards/dashboards/geodbdashboard.py
@@ -1,43 +1,89 @@
-from dash import html
+import pandas as pd
+from dash import html, dcc, Output, Input
 from dash import Dash
-
 from doors_dashboards.core.geodbaccess import get_points_from_geodb
-from doors_dashboards.components.scattermap import ScatterMapComponent
+from doors_dashboards.components.geodatascattermap import GeoScatterMapComponent
 
 DASHBOARD_ID = 'bulgarian_optical_data'
 
 
 def _create_app() -> Dash:
-    app = Dash(__name__)
+    app = Dash(__name__,suppress_callback_exceptions=True)
 
     points = get_points_from_geodb(
-        'bio-optical-data-K12_2019', 'io-bas',
-        variables=['chl-a [mg/m3]', 'temperature [°c]', 'salinity [psu]',
-                   'secchi disk [m]']
+        'bio-optical-data-K11_2019', 'io-bas',
+        variables=['chl-a [mg/m3]', 'temperature [°c]', 'salinity [psu]', 'secchi disk [m]', 'timestamp']
     )
 
-    scattermap = ScatterMapComponent().get(DASHBOARD_ID, points)
+    variables = ['chl-a [mg/m3]', 'temperature [°c]', 'salinity [psu]', 'secchi disk [m]']
+    selected_variable_default = variables[0]
+
+    scattermap = GeoScatterMapComponent().get(DASHBOARD_ID, points, selected_variable_default)
 
     app.layout = html.Div(
         [
+            # Header Div
             html.Div(
-                scattermap,
+                html.Img(src='https://doors.viewer.brockmann-consult.de/config/logo.png', style={'width': '200px'}),
+                style={
+                    'backgroundColor': 'rgb(12, 80, 111)',
+                    'padding': '15px',
+                    'textAlign': 'left',
+                }
+            ),
+
+            # Main content Div
+            html.Div(
+                children=[
+                    # Date Picker Div
+                    html.Div(
+                        [
+                            html.Div("Select Type: ",
+                                     style={'fontSize': 'large', 'fontWeight': 'bold', 'paddingRight': '10px',
+                                            'paddingLeft': '10px',
+                                            'fontFamily': 'Roboto, Helvetica, Arial, sans-serif'}),
+                            dcc.Dropdown(
+                                id='variable-dropdown',
+                                options=[
+                                    {'label': variable, 'value': variable} for variable in variables
+                                ],
+                                value=selected_variable_default,
+                                style={'width': '300px', 'height': '30px'}
+                            )
+                        ]
+                    ),
+                    # Map
+                    html.Div(
+                        id=DASHBOARD_ID,
+                        children=[
+                            # Map Div
+                            scattermap,
+                        ]
+                    ),
+                ],
                 style={
                     'display': 'flex',
-                    'flexDirection': 'column',
-                    'width': '100vh',
-                    'height': '1000px',
-                    'alignItems': 'center',
+                    'flexDirection': 'column',  # Adjust to column layout
+                    'width': '100%',
+                    'height': '100vh',
                 }
-            )
-        ],
-        style={
-            'display': 'flex',
-            'flexDirection': 'row',
-            'width': '100%',
-            'height': '100vh'
-        }
+            ),
+        ]
     )
+
+    @app.callback(
+        Output(DASHBOARD_ID, 'children'),
+        [Input('variable-dropdown', 'value')]
+    )
+    def update_scattermap(selected_variable):
+        print(selected_variable)
+        if selected_variable != 'chl-a [mg/m3]':
+            updated_scattermap = GeoScatterMapComponent().get(DASHBOARD_ID, points, selected_variable)
+            return updated_scattermap
+        else:
+            print('default', selected_variable_default)
+            return GeoScatterMapComponent().get(DASHBOARD_ID, points, selected_variable_default)
+
     return app
 
 
