16eaaa2668cf8da36d900c258c5c86df642ff120
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2024-01-16 17:41:04 +0100

    added function to get names of available geodb collections

--- doors_dashboards/core/geodbaccess.py
+++ doors_dashboards/core/geodbaccess.py
@@ -6,6 +6,25 @@ from xcube_geodb.core.geodb import GeoDBClient
 from .point import Point
 
 
+_GEODB_CLIENT = None
+
+
+def _get_client() -> GeoDBClient:
+    if _GEODB_CLIENT is None:
+        _GEODB_CLIENT = GeoDBClient(
+            auth_aud='https://xcube-users.brockmann-consult.de/api/v2'
+        )
+    global _GEODB_CLIENT
+    return _GEODB_CLIENT
+
+
+def get_collection_names_from_geodb():
+    geodb = _get_client()
+    df = geodb.get_my_collections()
+    doors_collections = df[df["database"].str.contains("doors") == True]
+    return list(doors_collections.collection)
+
+
 def get_points_from_geodb(
         collection: str, database: str, variables: List[str],
         name_of_time_column: str = 'timestamp'
@@ -30,9 +49,7 @@ def get_dataframe_from_geodb(
         collection: str, database: str, variables: List[str],
         name_of_time_column: str = 'timestamp'
 ) -> gpd.GeoDataFrame :
-    geodb = GeoDBClient(
-        auth_aud='https://xcube-users.brockmann-consult.de/api/v2'
-    )
+    geodb = _get_client()
     gdf = geodb.get_collection(collection, database=database)
     points_gdf = gdf[gdf["geometry"].geom_type == 'Point']
     points_gdf['lon'] = points_gdf.geometry.apply(lambda p: p.x)
