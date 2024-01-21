ccf50fb9c5f108c73c8754fc41d0f844e911917f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2024-01-21 15:18:05 +0100

    updated test

--- test/dashboards/test_dashboard.py
+++ test/dashboards/test_dashboard.py
@@ -6,8 +6,14 @@ from doors_dashboards.dashboards.dashboard import create_dashboard
 
 class DashboardTest(unittest.TestCase):
 
-    def test_create_dashboard(self):
+    def test_create_dashboard_1(self):
         with open("test_configs/test_config1.yml", "r") as tc:
+            config = yaml.safe_load(tc)
+            dashboard = create_dashboard(config)
+            dashboard.run_server(debug=True)
+
+    def test_create_dashboard_2(self):
+        with open("test_configs/test_config2.yml", "r") as tc:
             config = yaml.safe_load(tc)
             dashboard = create_dashboard(config)
             dashboard.run_server(debug=True)
