5948163ddb12ec433570da2ae4d3f5b8ff7a762a
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2024-01-26 12:39:29 +0100

    undo comment

--- test/dashboards/test_dashboard.py
+++ test/dashboards/test_dashboard.py
@@ -6,11 +6,11 @@ from doors_dashboards.dashboards.dashboard import create_dashboard
 
 class DashboardTest(unittest.TestCase):
 
-    # def test_create_dashboard_1(self):
-    #     with open("test_configs/config_with_geodb.yml", "r") as tc:
-    #         config = yaml.safe_load(tc)
-    #         dashboard = create_dashboard(config)
-    #         dashboard.run(debug=True)
+    def test_create_dashboard_1(self):
+        with open("test_configs/config_with_geodb.yml", "r") as tc:
+            config = yaml.safe_load(tc)
+            dashboard = create_dashboard(config)
+            dashboard.run(debug=True)
 
     def test_create_dashboard_2(self):
         with open("test_configs/meteogram_config.yml", "r") as tc:
